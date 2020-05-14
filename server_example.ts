a1bcf773c92c52acb0275e6b73cb4777bd50b04a
TMc42 <tmclean1212@gmail.com>
2020-05-14 17:35:28 -0500

    testing deno fmt command

--- server_example.ts
+++ server_example.ts
@@ -4,7 +4,7 @@
 import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 
 // Create a server instance ( not really since its just a func and not an oo class but whatever ) and tell it to listen on port 8000
-const s = serve({ port: 8000 }); 
+const s = serve({ port: 8000 });
 
 console.log("Serving...."); // Console.log just prints whatever you want it to in the console after you hit run
 console.log("http://localhost:8000/"); // Print the address we're serving to the console
@@ -13,7 +13,7 @@ console.log("http://localhost:8000/"); // Print the address we're serving to the
 // Cool thing about Deno is the builtin top level await
 // await pauses the async function until promise is fulfilled
 // You used to not be able to do it this easily
-for await (const req of s) { 
-    // the respond function can take any type of html you want
-    req.respond({ body: "<h1 style='color:red;'>Hello World</h1>\n" });
+for await (const req of s) {
+  // the respond function can take any type of html you want
+  req.respond({ body: "<h1 style='color:red;'>Hello World</h1>\n" });
 }
