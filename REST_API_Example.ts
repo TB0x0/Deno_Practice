1374431b3a04359b735b4164184e8479c8c3a496
TMc42 <tmclean1212@gmail.com>
2020-06-27 14:38:21 -0500

    Continuing oak example

--- REST_API_Example.ts
+++ REST_API_Example.ts
@@ -3,17 +3,81 @@
 // import application and router from Oak
 import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
 
-const env = Deno.env.toObject()
-const PORT = env.PORT || 4000
-const HOST = env.HOST || '127.0.0.1'
+const env = Deno.env.toObject();
+const PORT = env.PORT || 4000;
+const HOST = env.HOST || '127.0.0.1';
 
-// create Oak objects
-const router = new Router()
-const app = new Application()
+interface Book {
+    author: string;
+    title: string;
+    pub_date: number;
+}
 
-app.use(router.routes())
-app.use(router.allowedMethods())
 
-console.log(`Listening on port ${PORT}...`)
+let books: Array<Book> = [          // Create an array of Book interfaces
+    {
+        author: 'George Orwell',
+        title: '1984',
+        pub_date: 1949
+    },
+    {
+        author: 'Charles Dickens',
+        title: 'Great Expectations',
+        pub_date: 1860
+    },
+]
 
-await app.listen(`${HOST}:${PORT}`)
+export const getBooks = ({ response }: { response: any}) =>
+{
+    response.body = books;
+}
+
+export const getBook = ({
+    params,
+    response,
+}: {
+    params: {
+        title: string
+    }
+    response: any
+}) => {
+    const book = books.filter((book) => book.title === params.title)
+    if (book.length) {
+        response.status = 200;
+        response.body = book[0];
+        return;
+    }
+
+    response.status = 400;
+    response.body = { msg: `No record of ${params.title}` }
+}
+
+export const addBook = async ({
+    request,
+    response,
+}: {
+    request: any
+    response: any
+}) => {
+    const body = await request.body();
+    const { author, title, pub_date }: { author: string; title: string; pub_date: number } =
+    body.value;
+    books.push({
+        author: author,
+        title: title,
+        pub_date: pub_date,
+    })
+
+    response.body = { msg: 'ACCEPTED' };
+    response.status = 200;
+}
+// Create Oak objects
+const router = new Router();
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Listening on port ${PORT}...`);
+
+await app.listen(`${HOST}:${PORT}`);
